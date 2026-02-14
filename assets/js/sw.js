da05eb783f378fad9c16e1bbdaa8a8b3865bb921
Stef Wauters <wautersstef4@gmail.com>
2026-02-14 20:54:14 +0100

    working serving sw

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,12 +1,31 @@
+const CACHE_NAME = 'stef-cv-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/assets/images/Stef300x300-modified.webp',
+  '/assets/js/site-optimization.js'
+];
+
+// Installatie: Sla de belangrijkste bestanden op in de cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('pwa-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/assets/js/site-optimization.js',
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch: Serveer vanuit cache, update op de achtergrond (Stale-while-revalidate)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
     })
   );
 });
